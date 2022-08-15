411f8eef756c83ce3bdeb4244361267145a0f1d6
AyazAI <ayaz.uiit3@gmail.com>
2022-08-15 13:13:44 +0500

    fixes

--- startup-script.sh
+++ startup-script.sh
@@ -18,8 +18,8 @@ set -ev
 # [START getting_started_gce_startup_script]
 # Install or update needed software
 apt-get update
-apt-get install -yq git supervisor python python-pip python-distutils
-pip install --upgrade pip virtualenv
+apt-get install -yq git supervisor python3 python3-pip python3-distutils
+pip3 install --upgrade pip virtualenv
 
 # Fetch source code
 export HOME=/root
@@ -32,7 +32,7 @@ sudo bash /opt/app/add-google-cloud-ops-agent-repo.sh --also-install
 useradd -m -d /home/pythonapp pythonapp
 
 # Python environment setup
-virtualenv -p python /opt/app/env
+virtualenv -p python3 /opt/app/env
 /bin/bash -c "source /opt/app/env/bin/activate"
 /opt/app/env/bin/pip install -r /opt/app/requirements.txt
 
